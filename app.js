25d6337a4ca75e98ebaf8c68de5dd435d130d57e
shun midorikawa <shun0617_0402@yahoo.co.jp>
2020-04-02 20:33:51 +0900

    message

--- app.js
+++ app.js
@@ -16,12 +16,6 @@ var usersRouter = require('./routes/users');
 */
 var app = express();
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
- });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -70,5 +64,13 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+ });
+
 
 module.exports = app;
